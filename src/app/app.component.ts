0a6d8e72cf4e5028599d77b157c153d246f24475
Jean-Marie Cavet <jmcavet@yahoo.fr>
2025-01-16 13:05:15 +0100

    Initial commit of basic app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,121 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  addDoc,
+  collection,
+  collectionData,
+  deleteDoc,
+  doc,
+  Firestore,
+} from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TreeNode } from 'primeng/api';
+import { Tree } from 'primeng/tree';
+import { DialogModule } from 'primeng/dialog';
+
+interface Item {
+  id: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    Tree,
+    DialogModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'famappy';
+  files: TreeNode[] = [
+    {
+      key: '0',
+      label: 'Documents',
+      data: 'Documents Folder',
+      icon: 'pi pi-fw pi-inbox',
+      children: [
+        {
+          key: '0-0',
+          label: 'Work',
+          data: 'Work Folder',
+          icon: 'pi pi-fw pi-cog',
+          children: [
+            {
+              key: '0-0-0',
+              label: 'Expenses.doc',
+              icon: 'pi pi-fw pi-file',
+              data: 'Expenses Document',
+            },
+            {
+              key: '0-0-1',
+              label: 'Resume.doc',
+              icon: 'pi pi-fw pi-file',
+              data: 'Resume Document',
+            },
+          ],
+        },
+        {
+          key: '0-1',
+          label: 'Home',
+          data: 'Home Folder',
+          icon: 'pi pi-fw pi-home',
+          children: [
+            {
+              key: '0-1-0',
+              label: 'Invoices.txt',
+              icon: 'pi pi-fw pi-file',
+              data: 'Invoices for this month',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+  message: string = '';
+
+  visible: boolean = false;
+
+  showDialog() {
+    this.visible = true;
+  }
+
+  firestore = inject(Firestore);
+  itemCollection = collection(this.firestore, 'items');
+  item$ = collectionData<any>(this.itemCollection, { idField: 'id' });
+
+  items?: Item[];
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.item$.subscribe((data) => {
+      this.items = data;
+    });
+  }
+
+  onSubmit() {
+    addDoc(this.itemCollection, { text: this.message });
+    this.message = '';
+  }
+
+  onDelete(docId: string): Promise<void> {
+    console.log('docId: ', docId);
+    const docRef = doc(this.firestore, 'items', docId);
+
+    return deleteDoc(docRef);
+  }
+
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    element?.classList.toggle('my-app-dark');
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,47 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { MyPreset } from './mypreset';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'famappy-6181c',
+        appId: '1:998789579438:web:77674387cf299adfdda33f',
+        storageBucket: 'famappy-6181c.firebasestorage.app',
+        apiKey: 'AIzaSyCFHTJvWi_BqBV3fwXB8lRrO8WbGoP0qxc',
+        authDomain: 'famappy-6181c.firebaseapp.com',
+        messagingSenderId: '998789579438',
+        measurementId: 'G-N73T1JEWL7',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+        options: {
+          prefix: 'p',
+          darkModeSelector: '.my-app-dark',
+          cssLayer: false,
+        },
+      },
+      ripple: true,
+    }),
+  ],
 };
