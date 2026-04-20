f2fc45032765e3e146510fb9eecd4ba5b6772c8e
Jean-Marie Cavet <jmcavet@yahoo.fr>
2026-04-20 21:43:37 +0200

    refactor(tab-definition): move logic into new facade

--- src/app/features/new-recipe/components/tabs/tabs.component.ts
+++ src/app/features/new-recipe/components/tabs/tabs.component.ts
@@ -4,14 +4,16 @@ import {
   ContentChildren,
   inject,
   QueryList,
+  signal,
 } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 import { CommonModule } from '@angular/common';
 import { RecipeStateService } from '../../../../services/state/recipe.service';
+import { SegmentedControlComponent } from '../../../../shared/ui/segmented-control/segmented-control.component';
 
 @Component({
   selector: 'app-tabs',
-  imports: [CommonModule],
+  imports: [CommonModule, SegmentedControlComponent],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.css',
 })
@@ -24,18 +26,23 @@ export class TabsComponent {
   /** Declaration of local signals */
   recipeState = this.recipeService.recipeState;
 
+  readonly tabsTitles = signal<string[]>([]);
+
   /** Declaration of recipe state signals */
   readonly selectedTabTitle = computed(
-    () => this.recipeState().selectedTabTitle
+    () => this.recipeState().selectedTabTitle,
   );
 
-  ngAfterContentInit() {
+  toggleTab(tabTitle: string) {
+    this.recipeService.updateProperty('selectedTabTitle', tabTitle);
     this.activateSelectedTab();
   }
 
-  selectTab(tab: TabComponent) {
-    this.recipeService.updateProperty('selectedTabTitle', tab.tabTitle);
+  ngAfterContentInit() {
     this.activateSelectedTab();
+
+    // Set initially
+    this.tabsTitles.set(this.tabs?.toArray().map((tab) => tab.tabTitle) ?? []);
   }
 
   activateSelectedTab() {
