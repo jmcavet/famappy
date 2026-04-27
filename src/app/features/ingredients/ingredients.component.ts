d6560ef49cd92339575cbbed404ab9143014a4e6
Jean-Marie Cavet <jmcavet@yahoo.fr>
2026-04-27 21:35:07 +0200

    refactor(ingredients): move logic into new facade

--- src/app/features/ingredients/ingredients.component.ts
+++ src/app/features/ingredients/ingredients.component.ts
@@ -1,79 +1,24 @@
-import { NgFor, NgIf, Location } from '@angular/common';
-import {
-  Component,
-  computed,
-  effect,
-  ElementRef,
-  inject,
-  Signal,
-  signal,
-  ViewChild,
-} from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { LoadingComponent } from '../../shared/components/loading/loading.component';
-import { IngredientBackendService } from '../../services/backend/ingredient.service';
-import { IngredientCategoryBackendService } from '../../services/backend/ingredient-category.service';
-import { RecipeStateService } from '../../services/state/recipe.service';
-import { IngredientDocInBackend } from '../../models/ingredient.model';
-import { IngredientTypeWithDate } from '../../models/ingredient-type.model';
+import { IngredientsFacade } from './ingredients.facade';
 
 @Component({
   selector: 'app-ingredients',
-  imports: [FormsModule, NgFor, LoadingComponent, RouterLink],
+  imports: [FormsModule, LoadingComponent, RouterLink],
+  providers: [IngredientsFacade],
   templateUrl: './ingredients.component.html',
   styleUrl: './ingredients.component.css',
 })
 export class IngredientsComponent {
-  private location = inject(Location);
+  private facade = inject(IngredientsFacade);
 
-  /** Services */
-  private ingredientService = inject(IngredientBackendService);
-  private ingredientCategoryService = inject(IngredientCategoryBackendService);
-  private recipeService = inject(RecipeStateService);
-
-  /** Declaration of signals communicating with firestore */
-  readonly dbIngredients: Signal<IngredientDocInBackend[]> =
-    this.ingredientService.ingredients;
-  readonly isLoadingIngredients = this.ingredientService.loading;
-
-  readonly dbIngredientCategories: Signal<IngredientTypeWithDate[]> =
-    this.ingredientCategoryService.ingredientCategories;
-  readonly isLoadingIngredientCategories =
-    this.ingredientCategoryService.loading;
-
-  /** Declaration of local signals */
-  recipeState = this.recipeService.recipeState;
-  inputText = signal<string>('');
+  inputText = this.facade.inputText;
+  pageIsLoading = this.facade.pageIsLoading;
+  ingredientsFiltered = this.facade.ingredientsFiltered;
 
   @ViewChild('ingredientInput') ingredientInput!: ElementRef<HTMLInputElement>;
-
-  /** Define the ingredients filtered by the text entered by user */
-  readonly ingredientsFiltered = computed(() => {
-    const ingredients = this.dbIngredients().map((ingredient) => {
-      const typeName =
-        this.dbIngredientCategories().find(
-          (t) => t.id === ingredient.categoryId
-        )?.name || '';
-      return { ...ingredient, typeName };
-    });
-
-    const ingredientsFilteredBySearch = ingredients.filter((ingredient) => {
-      return ingredient.name
-        .toLowerCase()
-        .includes(this.inputText().toLowerCase());
-    });
-
-    return ingredientsFilteredBySearch;
-  });
-
-  constructor() {
-    /** Whenever users click on the back button (left arrow in header), they want to go back
-     * to the 'ingredients' tab (and not back to the 'definition' tab). We must preserve the state once
-     * the component has been initialized.
-     */
-    this.recipeService.preserveState(true);
-  }
 
   /** Access the input field and focus it on view initialization */
   ngAfterViewInit(): void {
@@ -83,13 +28,10 @@ export class IngredientsComponent {
   }
 
   onResetInput() {
-    this.inputText.set('');
+    this.facade.resetInput();
   }
 
   selectIngredient(ingredientId: string) {
-    this.recipeService.selectIngredient(ingredientId);
-    this.recipeService.mustPreserveState.set(true);
-    // queueMicrotask(() => this.location.back());
-    this.location.back();
+    this.facade.selectIngredient(ingredientId);
   }
 }
---
+++ src/app/features/ingredients/ingredients.facade.ts
@@ -0,0 +1,88 @@
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { RecipeStateService } from '../../services/state/recipe.service';
+import { Location } from '@angular/common';
+import { IngredientDomainFacade } from '../../domain-facades/ingredient.facade';
+import { IngredientCategoryDomainFacade } from '../../domain-facades/ingredientCategory.facade';
+
+@Injectable()
+export class IngredientsFacade {
+  /* ================================
+   * Dependencies
+   * ================================ */
+  /** Framework dependencies */
+  private location = inject(Location);
+
+  /** Domain access (business state & actions) */
+  private ingredientDomainFacade = inject(IngredientDomainFacade);
+  private ingredientCategoryDomainFacade = inject(
+    IngredientCategoryDomainFacade,
+  );
+
+  /** Transitional state */
+  private recipeService = inject(RecipeStateService);
+
+  /* ================================
+   * Domain-derived state
+   * ================================ */
+  readonly dbIngredients = this.ingredientDomainFacade.dbIngredients;
+  readonly ingredientsLoading = this.ingredientDomainFacade.ingredientsLoading;
+
+  readonly dbIngredientCategories =
+    this.ingredientCategoryDomainFacade.dbIngredientCategories;
+  readonly ingredientCategoriesLoading =
+    this.ingredientCategoryDomainFacade.ingredientCategoriesLoading;
+
+  constructor() {
+    /** Whenever users click on the back button (left arrow in header), they want to go back
+     * to the 'ingredients' tab (and not back to the 'definition' tab). We must preserve the state once
+     * the component has been initialized.
+     */
+    this.recipeService.preserveState(true);
+  }
+
+  /* ================================
+   * Local state
+   * ================================ */
+  /** Signals rendered on UI */
+  inputText = signal<string>('');
+
+  /* ================================
+   * Local derived state
+   * ================================ */
+  /** Public signals */
+  readonly pageIsLoading = computed(
+    () => this.ingredientsLoading() || this.ingredientCategoriesLoading(),
+  );
+
+  /** Define the ingredients filtered by the text entered by user */
+  readonly ingredientsFiltered = computed(() => {
+    const ingredients = this.dbIngredients().map((ingredient) => {
+      const typeName =
+        this.dbIngredientCategories().find(
+          (t) => t.id === ingredient.categoryId,
+        )?.name || '';
+      return { ...ingredient, typeName };
+    });
+
+    const ingredientsFilteredBySearch = ingredients.filter((ingredient) => {
+      return ingredient.name
+        .toLowerCase()
+        .includes(this.inputText().toLowerCase());
+    });
+
+    return ingredientsFilteredBySearch;
+  });
+
+  /* ================================
+   * PUBLIC API
+   * ================================ */
+  resetInput() {
+    this.inputText.set('');
+  }
+
+  selectIngredient(ingredientId: string) {
+    this.recipeService.selectIngredient(ingredientId);
+    this.recipeService.mustPreserveState.set(true);
+    this.location.back();
+  }
+}
