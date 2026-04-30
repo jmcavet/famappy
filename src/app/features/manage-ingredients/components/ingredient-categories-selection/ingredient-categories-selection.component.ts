d87e6612b2f733eab02da5b2292171e1a530edde
Jean-Marie Cavet <jmcavet@yahoo.fr>
2026-04-30 22:52:49 +0200

    refactor(ingredient-categories-selection): move logic into new facade

--- src/app/features/manage-ingredients/components/ingredient-categories-selection/ingredient-categories-selection.component.ts
+++ src/app/features/manage-ingredients/components/ingredient-categories-selection/ingredient-categories-selection.component.ts
@@ -1,53 +1,32 @@
-import { Component, computed, inject, signal, Signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
-import { IngredientCategoryBackendService } from '../../../../services/backend/ingredient-category.service';
-import {
-  IngredientType,
-  IngredientTypeWithDate,
-} from '../../../../models/ingredient-type.model';
+import { IngredientType } from '../../../../models/ingredient-type.model';
 import { ButtonComponent } from '../../../../shared/ui/button/button.component';
 import { ChipComponent } from '../../../../shared/ui/chip/chip.component';
+import { IngredientCategoriesSelectionFacade } from './ingredient-categories-selection.facade';
 
 @Component({
   selector: 'app-ingredient-categories-selection-page',
   imports: [ButtonComponent, ChipComponent, RouterLink],
+  providers: [IngredientCategoriesSelectionFacade],
   templateUrl: './ingredient-categories-selection.component.html',
   styleUrl: './ingredient-categories-selection.component.css',
 })
 export class IngredientCategoriesSelectionComponent {
-  /** Services */
-  private ingredientCategoryService = inject(IngredientCategoryBackendService);
+  private facade = inject(IngredientCategoriesSelectionFacade);
 
-  /** Declaration of signals communicating with firestore */
-  readonly ingredientCategories: Signal<IngredientTypeWithDate[]> =
-    this.ingredientCategoryService.ingredientCategories;
-
-  /** Declaration of local signals */
-  ingredientCategorySelected = signal<IngredientType | undefined>(undefined);
-
-  linkText = computed(() => {
-    const ingredientCategoriesNew = this.ingredientCategories().map((item) => ({
-      name: item.name,
-      id: item.id,
-    }));
-    const linkText =
-      ingredientCategoriesNew.length === 0
-        ? 'Start by adding a new category'
-        : '';
-    return linkText;
-  });
+  readonly ingredientCategoriesSorted = this.facade.ingredientCategoriesSorted;
+  readonly ingredientCategorySelected = this.facade.ingredientCategorySelected;
+  readonly linkText = this.facade.linkText;
 
   selectIngredientType(
     ingredientTypeElement: IngredientType,
     atLeastOneUnitSelected: boolean,
   ) {
-    this.ingredientCategorySelected.set(
-      atLeastOneUnitSelected ? undefined : ingredientTypeElement,
-    );
-
-    this.ingredientCategoryService.setSelectedIngredientCategory(
-      this.ingredientCategorySelected(),
+    this.facade.selectIngredientType(
+      ingredientTypeElement,
+      atLeastOneUnitSelected,
     );
   }
 }
