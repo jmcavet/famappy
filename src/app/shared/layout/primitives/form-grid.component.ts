0d4b8870d9f190fefd61ecaa09ad275877fa359a
Jean-Marie Cavet <jmcavet@yahoo.fr>
2026-06-04 22:24:52 +0200

    refactor(primitive-layouts): extend properties of form grid with max-content

--- src/app/shared/layout/primitives/form-grid.component.ts
+++ src/app/shared/layout/primitives/form-grid.component.ts
@@ -8,15 +8,26 @@ import { Component, computed, input } from '@angular/core';
   styles: [':host { display: grid; }'],
 })
 export class FormGridComponent {
+  maxContent = input<'max-content_1fr' | '1fr_max-content'>('max-content_1fr');
   gap = input<'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl'>('md');
   align = input<'start' | 'center' | 'end'>('start');
   justifyItems = input<'start' | 'center' | 'end' | 'stretch'>('start');
   responsive = input<boolean>(false);
 
+  private readonly colsMap = {
+    'max-content_1fr': 'grid-cols-[max-content_1fr]',
+    '1fr_max-content': 'grid-cols-[1fr_max-content]',
+  };
+
+  private readonly colsMapResponsive = {
+    'max-content_1fr': 'sm:grid-cols-[max-content_1fr]',
+    '1fr_max-content': 'sm:grid-cols-[1fr_max-content]',
+  };
+
   hostClasses = computed(() => {
     const gridCols = this.responsive()
-      ? 'grid-cols-1 sm:grid-cols-[max-content_1fr]'
-      : 'grid-cols-[max-content_1fr]';
+      ? `grid-cols-1 ${this.colsMapResponsive[this.maxContent()]}`
+      : this.colsMap[this.maxContent()];
 
     return [
       'grid',
