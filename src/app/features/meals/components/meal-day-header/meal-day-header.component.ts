52b94b0ba8b7b36ef9fe5081060c690adc552907
Jean-Marie Cavet <jmcavet@yahoo.fr>
2026-06-11 21:02:31 +0200

    refactor(meal-day-card): improve use of primitives

--- src/app/features/meals/components/meal-day-header/meal-day-header.component.ts
+++ src/app/features/meals/components/meal-day-header/meal-day-header.component.ts
@@ -1,13 +1,12 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CapitalizePipe } from '../../../../shared/pipes/capitalize.pipe';
-import { MealAddingOptionsComponent } from '../meal-adding-options/meal-adding-options.component';
 import { MealWithId } from '../../state/mealCart.model';
 import { CalendarDay } from '../calendar/calendar.facade';
 import { RowComponent } from '../../../../shared/layout/primitives/row.component';
 
 @Component({
   selector: 'app-meal-day-header',
-  imports: [CapitalizePipe, MealAddingOptionsComponent, RowComponent],
+  imports: [CapitalizePipe, RowComponent],
   templateUrl: './meal-day-header.component.html',
   styleUrl: './meal-day-header.component.css',
 })
