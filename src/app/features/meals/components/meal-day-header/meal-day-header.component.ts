4f7a70919d74f2dd56212da6fb382d8ad0085d50
Jean-Marie Cavet <jmcavet@yahoo.fr>
2026-06-11 12:04:08 +0200

    refactor(meals): add primities to current layout

--- src/app/features/meals/components/meal-day-header/meal-day-header.component.ts
+++ src/app/features/meals/components/meal-day-header/meal-day-header.component.ts
@@ -3,10 +3,11 @@ import { CapitalizePipe } from '../../../../shared/pipes/capitalize.pipe';
 import { MealAddingOptionsComponent } from '../meal-adding-options/meal-adding-options.component';
 import { MealWithId } from '../../state/mealCart.model';
 import { CalendarDay } from '../calendar/calendar.facade';
+import { RowComponent } from '../../../../shared/layout/primitives/row.component';
 
 @Component({
   selector: 'app-meal-day-header',
-  imports: [CapitalizePipe, MealAddingOptionsComponent],
+  imports: [CapitalizePipe, MealAddingOptionsComponent, RowComponent],
   templateUrl: './meal-day-header.component.html',
   styleUrl: './meal-day-header.component.css',
 })
