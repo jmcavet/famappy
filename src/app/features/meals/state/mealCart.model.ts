bca92f25ab4507525d47588c6657501bed809bd5
Jean-Marie Cavet <jmcavet@yahoo.fr>
2026-06-15 23:11:22 +0200

    refactor(meals): update manual recipe with new instructions property

--- src/app/features/meals/state/mealCart.model.ts
+++ src/app/features/meals/state/mealCart.model.ts
@@ -9,7 +9,11 @@ export interface MealDocInBackend {
   mealType: MealType;
   servings: number;
   cookId: string | null;
-  manualRecipe: { name: string; ingredients: string[] } | null;
+  manualRecipe: {
+    name: string;
+    ingredients: string[];
+    instructions: string[];
+  } | null;
 }
 
 // Used to add the document ID of the firestore object in order to eventually later on remove the Recipe by id
