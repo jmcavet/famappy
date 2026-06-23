50858509efc547cdb6dc9101191bbc7dc6acb4ca
Jean-Marie Cavet <jmcavet@yahoo.fr>
2026-06-23 21:58:08 +0200

    refactor(manage-cuisines): add primitives to layout

--- src/app/features/manage-cuisines/manage-cuisines.component.ts
+++ src/app/features/manage-cuisines/manage-cuisines.component.ts
@@ -3,20 +3,39 @@ import { CapitalizePipe } from '../../shared/pipes/capitalize.pipe';
 import { ManageCuisinesFacade } from './manage-cuisines.facade';
 import { LoadingComponent } from '../../shared/layout/overlays/loading/loading.component';
 import { FloatingButtonComponent } from '../../shared/layout/shell/floating-button/floating-button.component';
+import { HeaderShellComponent } from '../../shared/layout/shell/header-shell.component';
+import { PageLayoutComponent } from '../../shared/layout/primitives/page-layout.component';
+import { SectionComponent } from '../../shared/layout/primitives/section.component';
+import { StackComponent } from '../../shared/layout/primitives/stack.component';
+import { RowComponent } from '../../shared/layout/primitives/row.component';
+import { InlineComponent } from '../../shared/layout/primitives/inline.component';
+import { ButtonComponent } from '../../shared/ui/button/button.component';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-manage-cuisines',
-  imports: [CapitalizePipe, LoadingComponent, FloatingButtonComponent],
+  imports: [
+    CapitalizePipe,
+    LoadingComponent,
+    HeaderShellComponent,
+    PageLayoutComponent,
+    SectionComponent,
+    StackComponent,
+    RowComponent,
+    InlineComponent,
+    ButtonComponent,
+  ],
   providers: [ManageCuisinesFacade],
   templateUrl: './manage-cuisines.component.html',
   styleUrl: './manage-cuisines.component.css',
 })
 export class ManageCuisinesComponent {
+  private location = inject(Location);
   private facade = inject(ManageCuisinesFacade);
 
   /** Declaration of signals communicating with firestore */
-  readonly dbCuisines = this.facade.dbCuisines;
-  readonly canShowPage = this.facade.canShowPage;
+  readonly dbCuisinesSorted = this.facade.dbCuisinesSorted;
+  readonly pageIsLoading = this.facade.pageIsLoading;
 
   openAddCuisineInputModal(event: MouseEvent) {
     this.facade.openAddCuisineInputModal(event);
@@ -28,5 +47,9 @@ export class ManageCuisinesComponent {
 
   openDeleteModal(event: MouseEvent, cuisineId: string) {
     this.facade.openDeleteModal(event, cuisineId);
+  }
+
+  goBack() {
+    this.location.back();
   }
 }
