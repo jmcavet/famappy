4a496d60ca7e627fcfeb1644fc961e8beaf214b9
Jean-Marie Cavet <jmcavet@yahoo.fr>
2026-06-27 14:57:39 +0200

    init(shopping-ingredients-selection): initialize component files

---
+++ src/app/features/shopping/shopping-ingredients-selection/shopping-ingredients-selection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-ingredients-selection',
+  imports: [],
+  templateUrl: './shopping-ingredients-selection.component.html',
+  styleUrl: './shopping-ingredients-selection.component.css'
+})
+export class ShoppingIngredientsSelectionComponent {
+
+}
